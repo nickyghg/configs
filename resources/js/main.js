b8dabcb2820283e1ddcf67d08f726fad25b18eef
nickyghg <hallobrawl33@gmail.com>
2025-01-08 11:38:55 +0100

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -31,7 +31,7 @@ const configs = [
     tags: ["Blatant", "MiniBlox", "recommended"],
     anticheat: ["No AC, Only Setbacks"],
     description: ["Fly Method, Very Blatant, Tutorial for usage"],
-    cover: "https://raw.githubusercontent.com/Project-XD/project-xd.github.io/master/resources/icons/BlocksMC.png",
+    cover: "https://avatars.githubusercontent.com/u/97767956?s=200&v=4",
     ip: "localhost",
     downloadLink: "https://lootdest.org/s?aDs31I7B",
     otherButtons:[["https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-installer.jar", "Download Forge"], ["https://loot-link.com/s?f9IiBcr5", "Download Liquidbounce"]],
